d855c89dbe808c3cb82d2e55e1b1077ee19aa76f
lukeblatti <lpblatti@gmail.com>
2020-10-22 20:47:22 -0400

    plot 1 and plot 2 R script and .png file

---
+++ plot2.R
@@ -0,0 +1,29 @@
+##Download Data
+library(dplyr)
+if (!file.exists("./data")){dir.create("./data")}
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(fileURL, destfile = "./data/PM25emissions.zip")
+unzip(zipfile = "./data/PM25emissions.zip", exdir = "./data")
+
+
+##Read into R
+NEI <- readRDS("./data/summarySCC_PM25.rds")
+SCC <- readRDS("./data/Source_Classification_Code.rds")
+
+##Summarize emissions data by year
+year_em <- NEI %>%
+    filter(fips == "24510") %>%
+    group_by(year) %>%
+    summarise(sum = sum(Emissions))
+
+total_em <- year_em[['sum']]
+
+##Create bar plot
+options(scipen = 999)
+png(file = 'plot2.png')
+barplot(height = total_em,
+        names.arg = c("1999", "2002", "2005", "2008"),
+        xlab = "Year", ylab = "Total Emissions (in tons)",
+        main = "Baltimore Emissions by Year",
+        ylim = c(0, 3500))
+dev.off()
